1b2e37f6bea2486a192c22c2507329964185ffc8
Yumi Lee <eo8um@naver.com>
2020-11-14 12:16:51 +0900

    express
    터미널로 express 설치
    명령어 : npm install express

---
+++ 4_Express1.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();  // 생성자
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('익스프레스 서버 테스트');
+});
+
+app.listen(port, () => {
+    console.log(`${port}포트로 서버 실행중...`);
+});
+
