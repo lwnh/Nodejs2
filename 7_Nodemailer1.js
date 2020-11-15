dcdf30ef121ff42a501e0d50072d47d67e4701d9
Yumi Lee <eo8um@naver.com>
2020-11-15 12:08:03 +0900

    Nodemailer - html로 내용 보내기

--- 7_Nodemailer1.js
+++ 7_Nodemailer1.js
@@ -14,7 +14,8 @@ const mailOptions = {
     from: "홍길동 <eo8umm@gmail.com>",
     to: "김길동 eo8um@naver.com",
     subject: "node.js의 nodemailer 테스트중입니다.",
-    text: "안녕하세요. 메일이 잘 전달되나요???"
+    //text: "안녕하세요. 메일이 잘 전달되나요???"
+    html: "<h1>안녕하세요. 메일이 잘 전달되나요???</h1><p>정말 잘 되네요~~</p>"
 };
 
 transporter.sendMail(mailOptions, (err, info) => {
