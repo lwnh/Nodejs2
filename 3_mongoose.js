489b1ccd78ffae57ec3e3366f542044d060d167b
Yumi Lee <eo8um@naver.com>
2020-12-05 10:17:25 +0900

    mongoose 이용해 회원가입/로그인/회원조회

--- 3_mongoose.js
+++ 3_mongoose.js
@@ -81,6 +81,79 @@ router.route('/user/regist').post((req, res) => {
     }
 });
 
+router.route('/user/login').post((req, res) => {
+    console.log('/user/login 호출');
+    const userid = req.body.userid;
+    const userpw = req.body.userpw;
+
+    console.log(`parameter - userid:${userid}, userpw:${userpw}`);
+
+    if(database) {
+        loginUser(database, userid, userpw, (err, result) => {
+            if(err) {
+                console.log(err);
+            }
+            if(result) {
+                console.dir(result);
+                const username = result[0].name;
+                const gender = result[0].gender;
+                res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                res.write('<h2>로그인 성공</h2>');
+                res.write(`<p>아이디 : ${userid}</p>`);
+                res.write(`<p>비밀번호 : ${userpw}</p>`);
+                res.write(`<p>이름 : ${username}</p>`);
+                res.write(`<p>성별 : ${gender}</p>`);
+                res.end();
+            } else {
+                res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                res.write('<h2>로그인 실패</h2>');
+                res.write('<p>아이디 또는 비밀번호를 확인하세요.</p>');
+                res.end();
+            }
+        });
+    } else {
+        res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.write('<p>mongodb 데이터베이스에 연결하지 못했습니다.</p>');
+        res.end();
+    }
+});
+
+router.route('/user/list').get((req, res) => {
+    console.log('/user/list 호출');
+
+    if(database) {
+        UserModel.findAll((err, result) => {
+            if(err) {
+                console.log('리스트 조회 실패');
+            }
+            if(result) {
+                res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                res.write('<h2>회원 리스트</h2>');
+                res.write('<div><ul>');
+                for(let i=0; i<result.length; i++) {
+                    const userid = result[i].userid;
+                    const username = result[i].name;
+                    const gender = result[i].gender;
+                    res.write(`<li>${i} : ${userid} / ${username} / ${gender}</li>`);
+                }
+                res.write('</ul></div>');
+                res.end();
+            } else {
+                res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                res.write('<h2>회원 리스트</h2>');
+                res.write('<p>회원 정보가 없습니다.</p>');
+                res.end();
+            }
+        });
+    } else {
+        res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.write('<p>mongodb 데이터베이스에 연결하지 못했습니다.</p>');
+        res.end();
+    }
+});
+
 const addUser = function(database, userid, userpw, name, gender, callback) {
     console.log('addUser 호출');
     const users = new UserModel({userid:userid, userpw:userpw, name:name, gender:gender});
@@ -92,6 +165,23 @@ const addUser = function(database, userid, userpw, name, gender, callback) {
         }
         console.log('회원 document가 추가되었습니다.');
         callback(null, result);
+    });
+}
+
+const loginUser = function(database, userid, userpw, callback) {
+    console.log('loginUser 호출');
+    UserModel.find({userid:userid, userpw:userpw}, (err, result) => {
+        if(err) {
+            callback(err, null);
+            return;
+        }
+        if(result.length > 0) {
+            console.log('일치하는 사용자를 찾음');
+            callback(null, result);
+        } else {
+            console.log('일치하는 사용자가 없음');
+            callback(null, null);
+        }
     });
 }
 
